bb14a7aa616f6ca44dae519353be65301d57f049
Sriram Sritha <srithasriram30@gmail.com>
2018-06-12 20:17:40 +0800

    Add arithmetic operations

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 var decimal = false;
+var result_string = "";
 
 $(".num").click(function() {
   $("span").append($(this).val());
+  console.log(result_string);
 });
 
 $(".clear").click(function() {
@@ -16,3 +18,49 @@ $(".clear").click(function() {
       decimal = true;
     }
   });
+
+$("#sum").click(function(){
+
+  result_string =  result_string + $("span").text() + "+";
+  $("span").empty();
+  console.log(result_string);
+});
+
+$("#difference").click(function(){
+
+  result_string =  result_string + $("span").text() + "-";
+  $("span").empty();
+  console.log(result_string);
+
+});
+
+$("#multiply").click(function(){
+
+  result_string =  result_string + $("span").text() + "*";
+  $("span").empty();
+  console.log(result_string);
+
+});
+
+$("#divide").click(function(){
+
+  result_string =  result_string + $("span").text() + "/";
+  $("span").empty();
+  console.log(result_string);
+});
+
+$("#clear").click(function(){
+
+  $("span").empty();
+  console.log(result_string);
+});
+
+
+$("#result").click(function(){
+
+  var final_number = $("span").text()
+  $("span").empty();
+  result_string =  result_string + final_number;
+  $("span").append(eval(result_string));
+  result_string = "";
+});
